a1fe42cabdae92e315bfa4525d61cc39390ccf6f
yunush1 <yunushkh9@gmail.com>
2025-09-29 01:53:39 +0530

    sds

--- services/exchange/categoryService.js
+++ services/exchange/categoryService.js
@@ -1,4 +1,4 @@
-const Category = require('../../models/category');
+const Category = require('../../models/Category');
 const logger = require('../../utils/logger');
 
 const createCategory = async (category) => {
